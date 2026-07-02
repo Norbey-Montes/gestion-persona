8feacc040738ee874e49e597e906b6af5bab3ba6
Norbey Montes Ramirez <mitchel.montes3523@gmail.com>
2026-07-02 13:58:38 -0500

    clase persona

--- clase/persona.php
+++ clase/persona.php
@@ -1,24 +1,17 @@
 <?php
+class persona
+{
 
-class Persona {
-    // Propiedad de la clase
     public $nombre;
+    public $edad;
+    public $correo;
 
-    // Método constructor para asignar el nombre al crear el objeto
-    public function __construct($nombre) {
-        $this->nombre = $nombre;
+    public function saludar()
+    {
+        return "hola mi nombre es : ";
     }
 
-    // Método para mostrar el saludo
-    public function saludar() {
-        echo "Hola, mi nombre es " . $this->nombre . ".";
-    }
 }
 
-// Crear una nueva instancia (objeto) de la clase Persona
-$persona = new Persona("Norbey");
-
-// Llamar al método saludar
-$persona->saludar();
 
 ?>
